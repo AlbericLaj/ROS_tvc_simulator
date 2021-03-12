b6037451d7c5ce5befb8d4d81dec406fe9346cf9
RaphLins <raphael.secondaire@gmail.com>
2021-03-12 21:40:24 +0100

    refractoring

--- bash_scripts/drone_pi_remote_launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./copy_on_pi.sh
-#export ROS_MASTER_URI=http://155.207.33.185:11311
-#export ROS_IP=155.207.33.185
-#roslaunch tvc_simulator drone_pi_remote_launch.launch
-
-ssh pi@raspberrypi.local "source /home/pi/ros_catkin_ws/devel/setup.bash; roslaunch tvc_simulator drone_pi_launch.launch"
